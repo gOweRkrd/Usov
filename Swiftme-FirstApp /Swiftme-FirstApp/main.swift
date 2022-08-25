a282be8dc22dbe420d3f78705a134692e478bbe2
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2022-08-25 21:02:17 +0300

    Swiftme-FirstApp
    Swiftme-FirstApp

---
+++ Swiftme-FirstApp /Swiftme-FirstApp/main.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+print("Hello, World!")
+
+print("Введите первое значение")
+// получение первого значения
+var a = readLine()
+print("Введите второе значение")
+// получение второго значения
+var b = readLine()
+
+let result = sum(a,_b: b)
+print("Результат сложения - \(result)"
+      )
---
+++ Swiftme-FirstApp /func swift.swift
@@ -0,0 +1,13 @@
+//
+//  func swift.swift
+//  Swiftme-FirstApp
+//
+//  Created by Мария Родионова on 15.06.2022.
+//
+
+import Foundation
+
+
+func sum(_ a : String?,_b :String?) -> Int {
+    return Int (a!)!+Int (b!)!
+}
